179034e334e928642e98029b1c1f7bf044f4e0e3
rafaelcorreia94870 <a94870@alunos.uminho.pt>
2024-03-01 16:44:04 +0000

    aaa

--- projeto/src/Scene/scene.cpp
+++ projeto/src/Scene/scene.cpp
@@ -14,11 +14,15 @@
 #include "../Primitive/primitive.hpp"
 #include "../Primitive/Geometry/mesh.hpp"
 #include "../Primitive/BRDF/Phong.hpp"
+#include <filesystem>
+#include "../Primitive/Geometry/triangle.hpp"
+namespace fs = std::filesystem;
 #elif __unix__ || __unix || __linux__ || __APPLE__
 #include "tiny_obj_loader.h"
 #include "primitive.hpp"
 #include "mesh.hpp"
 #include "Phong.hpp"
+#include "triangle.hpp"
 #else
 #error "Unsupported operating system"
 #endif
@@ -79,7 +83,89 @@ bool Scene::Load (const std::string &fname) {
     
     //PrintInfo (myObjReader);
 
-    // convert loader's representation to my representation
+    #define TINYOBJLOADER_IMPLEMENTATION // define this in only *one*
+
+
+    std::string inputfile = fname;
+    tinyobj::ObjReaderConfig reader_config;
+#ifdef _WIN32
+    fs::path currentPath = fs::current_path();
+    fs::path path = currentPath / ".." / "src" / "Scene" / "tinyobjloader" / "models";
+    std::string pathStr = path.string();
+    reader_config.mtl_search_path = pathStr;
+#elif __unix__ || __unix || __linux__ || __APPLE__
+    reader_config = "/home/robert/aulas/4ano/2sem/VI/TP/VI/projeto/src/Scene/tinyobjloader/models"
+#endif
+
+    tinyobj::ObjReader reader;
+
+    if (!reader.ParseFromFile(inputfile, reader_config)) {
+        if (!reader.Error().empty()) {
+            std::cerr << "TinyObjReader: " << reader.Error();
+        }
+        exit(1);
+    }
+
+    if (!reader.Warning().empty()) {
+        std::cout << "TinyObjReader: " << reader.Warning();
+    }
+
+    auto& attrib = reader.GetAttrib();
+    auto& shapes = reader.GetShapes();
+    auto& materials = reader.GetMaterials();
+    // Loop over shapes
+    for (size_t s = 0; s < shapes.size(); s++) {
+        // Loop over faces(polygon)
+        size_t index_offset = 0;
+        float v1, v2, v3;
+        Primitive p;
+        Geometry g;
+        int materialndx;
+        for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
+            size_t fv = size_t(shapes[s].mesh.num_face_vertices[f]);
+
+            // Loop over vertices in the face.
+            for (size_t v = 0; v < fv; v++) {
+                // access to vertex
+                tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+                tinyobj::real_t vx = attrib.vertices[3 * size_t(idx.vertex_index) + 0];
+                tinyobj::real_t vy = attrib.vertices[3 * size_t(idx.vertex_index) + 1];
+                tinyobj::real_t vz = attrib.vertices[3 * size_t(idx.vertex_index) + 2];
+                Point vertice = Point(vx,vy,vz);
+                //points.push_back(vertice);
+
+                // Check if `normal_index` is zero or positive. negative = no normal data
+                Vector normal = Vector(0, 0, 0);
+
+                if (idx.normal_index >= 0) {
+                    tinyobj::real_t nx = attrib.normals[3 * size_t(idx.normal_index) + 0];
+                    tinyobj::real_t ny = attrib.normals[3 * size_t(idx.normal_index) + 1];
+                    tinyobj::real_t nz = attrib.normals[3 * size_t(idx.normal_index) + 2];
+                    normal = Vector((float)nx, (float)ny, (float)nz);
+                }
+
+                // Check if `texcoord_index` is zero or positive. negative = no texcoord data
+                if (idx.texcoord_index >= 0) {
+                    tinyobj::real_t tu = attrib.texcoords[2 * size_t(idx.texcoord_index) + 0];
+                    tinyobj::real_t tv = attrib.texcoords[2 * size_t(idx.texcoord_index) + 1];
+                }
+
+                // Optional: vertex colors
+                // tinyobj::real_t red   = attrib.colors[3*size_t(idx.vertex_index)+0];
+                // tinyobj::real_t green = attrib.colors[3*size_t(idx.vertex_index)+1];
+                // tinyobj::real_t blue  = attrib.colors[3*size_t(idx.vertex_index)+
+            }
+            p.g = g;
+            p.material_ndx = materialndx;
+            this->prims.push_back(&p);
+
+            index_offset += fv;
+
+            // per-face material
+            shapes[s].mesh.material_ids[f];
+        }
+    }
+
 
     // your code here
     return true;
